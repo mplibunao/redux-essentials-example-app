d32ea0e116077383ce50217984de1ebc48119525
MP <markpaololibunao@gmail.com>
2024-02-20 14:09:01 +0800

    Creates a reusable PostAuthor component that displays the post author of a post using the redux store/selector

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PostAuthor from './PostAuthor'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
@@ -11,6 +12,9 @@ export const PostsList = () => {
       {posts.map((post) => (
         <article className="post-excerpt" key={post.id}>
           <h3>{post.title}</h3>
+          <div>
+            <PostAuthor userId={post.user} />
+          </div>
           <p className="post-content">{post.content.substring(0, 100)}</p>
           <Link to={`/posts/${post.id}`} className="button muted-button">
             View Post

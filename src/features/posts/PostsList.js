f7cb5084070c212e11280502b573aa7d964ee000
MP <markpaololibunao@gmail.com>
2024-02-21 16:35:30 +0800

    Displays the fetched posts along with a loading spinner

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,11 +5,31 @@ import { PostAuthor } from './PostAuthor'
 import { selectAllPosts, fetchPosts } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
+import { Spinner } from '../../components/Spinner'
+
+const PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt">
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
 
 export const PostsList = () => {
   const dispatch = useDispatch()
   const posts = useSelector(selectAllPosts)
   const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -17,26 +37,22 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const orderedPosts = [...posts].sort((a, b) => b.date.localeCompare(a.date))
+  let content
+  if (postStatus === 'loading') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = [...posts].sort((a, b) => b.date.localeCompare(a.date))
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {orderedPosts.map((post) => (
-        <article className="post-excerpt" key={post.id}>
-          <h3>{post.title}</h3>
-          <div>
-            <PostAuthor userId={post.user} />
-            <TimeAgo timestamp={post.date} />
-          </div>
-          <p className="post-content">{post.content.substring(0, 100)}</p>
-
-          <ReactionButtons post={post} />
-          <Link to={`/posts/${post.id}`} className="button muted-button">
-            View Post
-          </Link>
-        </article>
-      ))}
+      {content}
     </section>
   )
 }
